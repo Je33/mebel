9551059cd651385d08a2c8133795f3795b91838d
Je <jetapahob@gmail.com>
2013-01-11 23:50:58 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mebel::Application.config.secret_token = 'e09393eafc05bd4d337e26bce21dfd6c1cedf47f88740bec043796883fbda5e95fe5a56719e34e3d833d82f183430e112fedaadba26ba4ec925ded511c6f1146'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mebel::Application.config.session_store :cookie_store, key: '_mebel_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mebel::Application.config.session_store :active_record_store
