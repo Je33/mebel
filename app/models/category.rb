af3efece32f301fcc9b70d82c4f2532bd29e1dba
Je <jetapahob@gmail.com>
2013-02-26 13:55:06 +0400

    update

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,9 @@ class Category < ActiveRecord::Base
 
   has_many :products
 
-  has_attached_file :photo, :styles => {:s => '50x50', :m => '188x150'}
+  has_attached_file :photo,
+                    :styles => {:s => '50x50', :m => '188x150'},
+                    :path => "/:class/:attachment/:id_partition/:style/:filename",
+                    :url => "/:class/:attachment/:id_partition/:style/:filename"
 
 end
